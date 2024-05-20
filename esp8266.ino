e2154408e08547d28dea1b83800462959644ae6e
Mirza Fahad Zia <76650975+mediocre9@users.noreply.github.com>
2024-05-20 20:22:34 +0500

    refactor: improve code organization into class

--- esp8266.ino
+++ esp8266.ino
@@ -1,68 +1,68 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* NETWORK_NAME     = "It & Robotics - (Node-MCU)";
-const char* PASSWORD         = "1116equj5";
-const char* ON_SIGNAL_VAL    = "1";
-const char* OFF_SIGNAL_VAL   = "0";
+const char* NETWORK_NAME = "It & Robotics";
+const char* PASSWORD = "1116equj5";
+const char* ON_SIGNAL_VALUE = "1";
+const char* OFF_SIGNAL_VALUE = "0";
 unsigned BAUD_RATE = 9600;
 short PORT = 80;
 
-void initialize();
-void routes();
-void hasClientConnected();
-void onSignal();
-void offSignal();
+class SmartLocker {
+public: 
+    SmartLocker(): _server(PORT) {}
 
-ESP8266WebServer server(PORT);
+    void init() {
+        pinMode(BUILTIN_LED, OUTPUT);
+        digitalWrite(BUILTIN_LED, 1);
+        delay(1000);
+        Serial.begin(BAUD_RATE);
 
+        if (!WiFi.softAP(NETWORK_NAME, PASSWORD)) {
+            Serial.print("Failed to connect!");
+            return;
+        }
+
+        Serial.println("===============================================");
+        Serial.print("IP: ");
+        Serial.print(WiFi.softAPIP());
+    }
+
+    void setupRoutes() {
+        _server.on("/", [this]() {
+            _server.send(200, "text/plain", "Connected");
+        });
+
+        _server.on("/on_signal", [this]() {
+            digitalWrite(BUILTIN_LED, 0);
+            _server.send(200, "text/plain", ON_SIGNAL_VALUE);
+            Serial.println("Bit Status: 1");
+        });
+
+        _server.on("/off_signal", [this]() {
+            digitalWrite(BUILTIN_LED, 1);
+            _server.send(200, "text/plain", OFF_SIGNAL_VALUE);
+            Serial.println("Bit Status: 0");
+        });
+
+        _server.begin();
+    }
+
+    void requestHandler() {
+        _server.handleClient();
+    }
+
+private: 
+    ESP8266WebServer _server;
+};
+
+SmartLocker sm;
 
 void setup() {
-  initialize();
-  routes();
+    sm.init();
+    sm.setupRoutes();
 }
 
 void loop() {
-  server.handleClient();
+    sm.requestHandler();
 }
-
-void initialize() {
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, 1);
-  delay(1000);
-  Serial.begin(BAUD_RATE);
-  WiFi.softAP(NETWORK_NAME, PASSWORD);
-  
-  Serial.println("===============================================");
-  Serial.print("IP: ");
-  Serial.print(WiFi.softAPIP());
-}
-
-
-void routes() {
-  server.on("/", hasClientConnected);
-  server.on("/on_signal", onSignal);
-  server.on("/off_signal", offSignal);
-  server.begin();
-}
-
-
-void hasClientConnected(){
-  server.send(200, "text/plain", "Connected");
-}
-
-
-void onSignal() {
-  digitalWrite(BUILTIN_LED, 0);
-  server.send(200, "text/plain", ON_SIGNAL_VAL);
-  Serial.println("Bit Status: 1");
-}
-
-void offSignal() {
-  digitalWrite(BUILTIN_LED, 1);
-  server.send(200, "text/plain", OFF_SIGNAL_VAL);
-  Serial.println("Bit Status: 0");
-}
-
-
-
