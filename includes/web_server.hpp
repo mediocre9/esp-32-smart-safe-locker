a27e5d449d7d4d624ad320ee1024d319fa2f66ae
mediocre9 <mirzafahadzia9@gmail.com>
2025-01-23 19:29:53 +0500

    close #19 add endpoint protection, separate auto-lock feature with websockets

--- includes/web_server.hpp
+++ includes/web_server.hpp
@@ -1,8 +1,9 @@
+
 #ifndef WEB_SERVER_H
 #define WEB_SERVER_H
 
-#include "database.hpp"
-#include "firebase_operations.hpp"
+#include "../includes/database.hpp"
+#include "../includes/firebase_operations.hpp"
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 #include <map>
@@ -14,9 +15,8 @@ class WebServer
 public:
     static void start();
     static void setupRoutes();
-    static void setupSTAMode();
-    static bool isConnectedToWiFi();
-
+    static void setupDeviceNetworkModes();
+    static bool isConnectedToInternet();
     static void loginHandler_GET(AsyncWebServerRequest *request);
     static void loginHandler_POST(AsyncWebServerRequest *request);
     static void usersHandler_GET(AsyncWebServerRequest *request);
@@ -27,17 +27,17 @@ public:
     static void homeWifiHandler_POST(AsyncWebServerRequest *request);
     static void changePasswordHandler_GET(AsyncWebServerRequest *request);
     static void changePasswordHandler_POST(AsyncWebServerRequest *request);
-    static void lockController_POST(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total);
-
+    static void rebootDeviceHandler_GET(AsyncWebServerRequest *request);
+    static void lockController_GET(AsyncWebServerRequest *request);
     static void sendHTML(AsyncWebServerRequest *request, const String &filename);
-    static String generateSessionUUID();
+    static String createSession();
     static bool isAuthorized(AsyncWebServerRequest *request);
 
 public:
     static User users;
     static String sessionID;
     static CfgDatabase database;
-    static bool isWiFiConnected;
+    static bool isInternetConnected;
     static AsyncWebServer server;
     static FirebaseOperations firebase;
 };
