ba4d8f634a9ccabc1606b9970877130e7e352141
mediocre9 <mirzafahadzia9@gmail.com>
2025-01-23 19:34:35 +0500

    close #17, close #16 integrate WebSocket for lock control and enhance server setup

--- esp-32-smart-safe-locker.ino
+++ esp-32-smart-safe-locker.ino
@@ -1,30 +1,32 @@
 #include "includes/config.hpp"
 #include "includes/web_server.hpp"
 #include "includes/firebase_operations.hpp"
-#include <Arduino.h>
+#include "includes/web_socket.hpp"
 #include <LittleFS.h>
+#include <Arduino.h>
 
-WebServer server;
-FirebaseOperations firebase;
+WebSocket webSocket("/lock-controller");
 
-void initializeRelays()
+inline void initializeLockers()
 {
   for (const auto &i : _GPIO_PINS_)
   {
     pinMode(i.second, OUTPUT);
-    digitalWrite(i.second, LOW);
+    digitalWrite(i.second, HIGH);
   }
 }
 
 void setup()
 {
+  WebServer webServer;
+
   Serial.begin(BAUD_RATE);
-  initializeRelays();
+  initializeLockers();
 
   if (!LittleFS.begin())
   {
-    Serial.println("LittleFS Failed!");
-    Serial.println("Restarting . . . ");
+    LOGLN("LittleFS Failed!");
+    LOGLN("Restarting . . . ");
     ESP.restart();
     return;
   }
@@ -33,20 +35,28 @@ void setup()
   // should not be changed.
   // Otherwise the code will throw
   // runtime errors . . .
-  // because the STA mode was setup in
-  // [setupSTAMode] method
-  // and for settting the WiFi mode
-  // it should be at the beginning...
+  // because the STA and AP modes were used in
+  // [setupDeviceNetworkModes] method
+  // and these modes should be setup before starting the server...
   //
   // https://github.com/espressif/arduino-esp32/issues/8661#issuecomment-1731752332
-  server.setupSTAMode();
-  server.start();
-  server.setupRoutes();
+  webServer.setupDeviceNetworkModes();
+  webServer.start();
+  webServer.setupRoutes();
 
-  if (server.isConnectedToWiFi())
+  if (webServer.isConnectedToInternet())
   {
-    firebase.configure();
+    webServer.firebase.startAuthentication();
+    if (webServer.firebase.isAuthenticated())
+    {
+      webSocket.init();
+      webServer.server.addHandler(&(webSocket.getWebSocketInstance()));
+      webServer.firebase.listenForAuthorizationStatus();
+    }
   }
 }
 
-void loop() {}
+void loop()
+{
+  webSocket.getWebSocketInstance().cleanupClients();
+}
