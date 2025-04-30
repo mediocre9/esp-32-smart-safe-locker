20915339c8bbc005905543f7736c9fe8ab911b07
mediocre9 <mirzafahadzia9@gmail.com>
2025-04-30 15:02:06 +0500

    refactor: improve code formatting and consistency in `Database` and `CfgDatabase` classes

--- includes/database.hpp
+++ includes/database.hpp
@@ -1,24 +1,24 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-#include <Arduino.h>
+#include <WString.h>
 #include <map>
+
+#include "Config.hpp"
 
 using Cfg = std::map<String, String>;
 
 template <typename T>
-class Database
-{
+class Database {
 public:
-        virtual bool write(const String &filename, const T &data) = 0;
-        virtual T read(const String &filename) = 0;
+    [[nodiscard]] virtual bool write(const String& filename, const T& data) = 0;
+    [[nodiscard]] virtual T read(const String& filename) = 0;
 };
 
-class CfgDatabase : public Database<Cfg>
-{
+class CfgDatabase final : public Database<Cfg> {
 public:
-        bool write(const String &filename, const Cfg &data) override;
-        Cfg read(const String &filename) override;
+    [[nodiscard]] bool write(const String& filename, const Cfg& data) override;
+    [[nodiscard]] Cfg read(const String& filename) override;
 };
 
 #endif
